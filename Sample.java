fc5d7e1cdd50bc5299e856abfb60d1790a24efd8
Anand <yannamanenianandkumar@gmail.com>
2024-07-15 20:50:35 -0400

    Completed Trees-3

--- Sample.java
+++ Sample.java
@@ -5,3 +5,81 @@
 
 
 // Your code here along with comments explaining your approach
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+       this.result = new ArrayList<>();
+        helper(root, 0, targetSum, new ArrayList<>());
+        return result;
+    }
+
+    private void helper(TreeNode root, int currsum, int targetSum, List<Integer> path) {
+
+        if(root == null) return;
+
+        currsum += root.val;
+        path.add(root.val);
+        if(root.left == null && root.right == null) {
+            if(currsum == targetSum) {
+                result.add(new ArrayList<>(path));
+                //return;
+            }
+        }
+            helper(root.left,currsum,targetSum,new ArrayList<>(path));
+            helper(root.right,currsum,targetSum,new ArrayList<>(path));
+        
+    }
+}
+
+//----------------------------------------------
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            if (t1 == null && t2 == null) continue;
+            if (t1 == null || t2 == null) return false;
+            if (t1.val != t2.val) return false;
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
+        }
+        return true;
+    }
+}
